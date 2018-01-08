25cb5e832d109a956fe136981ec3c0750e3f13e7
Nathan Genetzky <nathan@genetzky.us>
2018-01-07 19:00:58 -0600

    trello: Create functions for upserting nested resources.

--- jatdb/trello/__main__.py
+++ jatdb/trello/__main__.py
@@ -7,23 +7,32 @@ from jatdb.trello.json import board_to_json
 from jatdb.trello.json import card_to_json
 from jatdb.trello.json import list_to_json
 
-def main(args=None):
-    c = get_client()
-
+def upsert_boards_from_client(c):
     boards_table = get_table('boards')
     boards = c.list_boards()
     for b in boards:
         upsert(board_to_json(b), boards_table)
 
-        lists_table = get_table('lists')
-        lists = b.get_lists('all')
-        for l in lists:
-            upsert(list_to_json(l), lists_table)
+def upsert_lists_from_board(b):
+    lists_table = get_table('lists')
+    lists = b.get_lists('all')
+    for l in lists:
+        upsert(list_to_json(l), lists_table)
 
-            cards_table = get_table('cards')
-            cards = l.list_cards('all')
-            for card in cards:
-                upsert(card_to_json(card), cards_table)
+def upsert_cards_from_list(l):
+    cards_table = get_table('cards')
+    cards = l.list_cards('all')
+    for card in cards:
+        upsert(card_to_json(card), cards_table)
+
+def main(args=None):
+    c = get_client()
+
+    upsert_boards_from_client(c)
+    for b in c.list_boards():
+        upsert_lists_from_board(b)
+        for l in b.get_lists('all'):
+            upsert_cards_from_list(l)
 
     return 0
 
